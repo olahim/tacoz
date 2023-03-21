a2dc58479c14a16e463d2299f8c78a78169d97a7
olahim <ayankoyaola@gmail.com>
2023-03-21 00:50:58 -0400

    Make changes to file

---
+++ tacoz-api/src/main/java/tacoz/web/api/IngredientResource.java
@@ -0,0 +1,22 @@
+package tacoz.web.api;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import lombok.Getter;
+import tacoz.Ingredient;
+import tacoz.Ingredient.Type;
+
+public class IngredientResource extends ResourceSupport {
+
+    @Getter
+    private String name;
+
+    @Getter
+    private Type type;
+
+    public IngredientResource(Ingredient ingredient) {
+        this.name = ingredient.getName();
+        this.type = ingredient.getType();
+    }
+
+}
